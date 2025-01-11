688dbe10c38efdb93e5f556d451f0be5d19e77ec
action <action@github.com>
2025-01-11 03:49:22 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Sat Jan 11 2025 03:45:43 GMT+0000 (Coordinated Universal Time)
+//Sat Jan 11 2025 03:49:22 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 if (event.budget.kjstatus === 1) {
@@ -14,7 +14,7 @@ if (event.budget.kjstatus === 1) {
       chrome.storage.local.set({
         "sendid": randomNum
       }, function () {});
-      var element_input = document.querySelector("textarea[class^=input]");
+      var element_input = document.querySelector(".send-comment-area input");
       element_input.click();
       if (element_input._valueTracker) var _templMethod = element_input._valueTracker.getValue;
       if (element_input.getValue) var _templMethod = element_input.getValue;
@@ -27,51 +27,63 @@ if (event.budget.kjstatus === 1) {
         });
         element_input.dispatchEvent(eventt);
         element_input._valueTracker && (element_input._valueTracker.getValue = _templMethod);
-        document.querySelector("[class^=text-area] [class^=button]").click();
+        setTimeout(function () {
+          document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+            "bubbles": true
+          }));
+        }, 200);
       }
     } else event.budget.sort >= num ? chrome.storage.local.set({
       "sort": 1
     }, function () {
-      var _0x5cdd00 = document.querySelector("textarea[class^=input]");
-      _0x5cdd00.click();
-      if (_0x5cdd00._valueTracker) {
-        var _0x4b0370 = _0x5cdd00._valueTracker.getValue;
-      }
-      if (_0x5cdd00.getValue) {
-        var _0x4b0370 = _0x5cdd00.getValue;
-      }
-      _0x5cdd00._valueTracker && (_0x5cdd00._valueTracker.getValue = () => "");
+      var _0xaca04d = document.querySelector(".send-comment-area input");
+      _0xaca04d.click();
+      if (_0xaca04d._valueTracker) var _0x27b0c8 = _0xaca04d._valueTracker.getValue;
+      if (_0xaca04d.getValue) var _0x27b0c8 = _0xaca04d.getValue;
+      _0xaca04d._valueTracker && (_0xaca04d._valueTracker.getValue = () => "");
       if (typeof arr[0] === "undefined") {} else {
-        _0x5cdd00.value = hj(arr[0]);
-        var _0x3d21f3 = new InputEvent("input", {
+        _0xaca04d.value = hj(arr[0]);
+        var eventt = new InputEvent("input", {
           "bubbles": true,
           "cancelable": true
         });
-        _0x5cdd00.dispatchEvent(_0x3d21f3);
-        if (_0x5cdd00._valueTracker) {
-          _0x5cdd00._valueTracker.getValue = _0x4b0370;
-        }
-        document.querySelector("[class^=text-area] [class^=button]").click();
+        _0xaca04d.dispatchEvent(eventt);
+        _0xaca04d._valueTracker && (_0xaca04d._valueTracker.getValue = _0x27b0c8);
+        setTimeout(function () {
+          document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+            "bubbles": true
+          }));
+        }, 200);
       }
     }) : chrome.storage.local.set({
       "sort": event.budget.sort + 1
     }, function () {
-      var _0x22780e = document.querySelector("textarea[class^=input]");
-      _0x22780e.click();
-      if (_0x22780e._valueTracker) var _0xc87f22 = _0x22780e._valueTracker.getValue;
-      if (_0x22780e.getValue) {
-        var _0xc87f22 = _0x22780e.getValue;
+      var _0x500e16 = document.querySelector(".send-comment-area input");
+      _0x500e16.click();
+      if (_0x500e16._valueTracker) {
+        var _0x4949e5 = _0x500e16._valueTracker.getValue;
       }
-      _0x22780e._valueTracker && (_0x22780e._valueTracker.getValue = () => "");
+      if (_0x500e16.getValue) {
+        var _0x4949e5 = _0x500e16.getValue;
+      }
+      if (_0x500e16._valueTracker) {
+        _0x500e16._valueTracker.getValue = () => "";
+      }
       if (typeof arr[event.budget.sort] === "undefined") {} else {
-        _0x22780e.value = hj(arr[event.budget.sort]);
-        var _0x2de573 = new InputEvent("input", {
-          "bubbles": true,
-          "cancelable": true
-        });
-        _0x22780e.dispatchEvent(_0x2de573);
-        _0x22780e._valueTracker && (_0x22780e._valueTracker.getValue = _0xc87f22);
-        document.querySelector("[class^=text-area] [class^=button]").click();
+        {
+          _0x500e16.value = hj(arr[event.budget.sort]);
+          var eventt = new InputEvent("input", {
+            "bubbles": true,
+            "cancelable": true
+          });
+          _0x500e16.dispatchEvent(eventt);
+          _0x500e16._valueTracker && (_0x500e16._valueTracker.getValue = _0x4949e5);
+          setTimeout(function () {
+            document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+              "bubbles": true
+            }));
+          }, 200);
+        }
       }
     });
   } else chrome.storage.local.set({
@@ -87,34 +99,26 @@ if (event.budget.spstatus === 1) {
     arras = arras.split(",");
     if (arras.length == 1) {
       var index = parseInt(arras[0]),
-        divheight = document.querySelector("[class^=render-item]").offsetHeight,
-        topdiv = document.querySelector("[class^=list-wrap]"),
-        asdiv = topdiv.querySelector("div");
+        divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+        asdiv = document.querySelector(".goods-wrapper");
       asdiv.scrollTop = divheight * index - divheight;
       setTimeout(function () {
-        var _0x594347 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-          _0x596b16 = document.querySelectorAll("[class^=render-item]");
-        for (var _0x530bf5 = 0; _0x530bf5 < _0x594347.length; _0x530bf5++) {
-          if (_0x594347[_0x530bf5].value == index) {
-            {
-              var _0x594347 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-                _0x5b52f1 = _0x596b16[_0x530bf5].querySelector("[class^=operate-area]").textContent;
-              if (_0x5b52f1 == "讲解") {
-                _0x596b16[_0x530bf5].querySelector("[class^=talking-btn]").click();
-              } else {
-                var _0x596b16 = document.querySelectorAll("[class^=render-item]");
-                _0x596b16[_0x530bf5].querySelector("[class^=talking-btn]").click();
-                setTimeout(function () {
-                  var _0x1744e2 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-                    _0x1d2de5 = document.querySelectorAll("[class^=render-item]");
-                  _0x1d2de5[_0x530bf5].querySelector("[class^=talking-btn]").click();
-                }, 1200);
-              }
+        var _0x158f96 = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+          _0x27dc4a = _0x158f96.querySelectorAll("input"),
+          _0x1d4233 = _0x158f96.querySelectorAll(".live-pro-card-draggable");
+        for (var _0x2b3770 = 0; _0x2b3770 < _0x27dc4a.length; _0x2b3770++) {
+          {
+            if (_0x27dc4a[_0x2b3770].value == index) {
+              var _0x1c2ac0 = _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+              _0x1c2ac0 == "开始讲解" ? (console.log(_0x1c2ac0), _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click()) : (console.log(_0x1c2ac0), _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                console.log(_0x1c2ac0, 23);
+                _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+              }, 2000));
               break;
             }
           }
         }
-      }, 1000);
+      }, 800);
     } else {
       var nums = arras.length - 1;
       let nows = new Date();
@@ -123,65 +127,46 @@ if (event.budget.spstatus === 1) {
           "spsort": 1
         }, function () {});
         var index = parseInt(arras[0]),
-          divheight = document.querySelector("[class^=render-item]").offsetHeight,
-          topdiv = document.querySelector("[class^=list-wrap]"),
-          asdiv = topdiv.querySelector("div");
+          divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+          asdiv = document.querySelector(".goods-wrapper");
         asdiv.scrollTop = divheight * index - divheight;
         setTimeout(function () {
-          var _0x52bdf9 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-            _0xc0dac3 = document.querySelectorAll("[class^=render-item]");
-          for (var _0x1bbaaf = 0; _0x1bbaaf < _0x52bdf9.length; _0x1bbaaf++) {
+          var _0x446428 = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+            _0x58805b = _0x446428.querySelectorAll("input"),
+            _0x20386c = _0x446428.querySelectorAll(".live-pro-card-draggable");
+          for (var _0x31b60c = 0; _0x31b60c < _0x58805b.length; _0x31b60c++) {
             {
-              if (_0x52bdf9[_0x1bbaaf].value == index) {
-                var _0x47f06f = _0xc0dac3[_0x1bbaaf].querySelector("[class^=operate-area]").textContent;
-                document.querySelector(".ant-modal.ant-modal-confirm.ant-modal-confirm-confirm.custom-modal");
-                if (_0x47f06f == "讲解") {
-                  _0xc0dac3[_0x1bbaaf].querySelector("[class^=talking-btn]").click();
-                } else {
-                  var _0xc0dac3 = document.querySelectorAll("[class^=render-item]");
-                  _0xc0dac3[_0x1bbaaf].querySelector("[class^=talking-btn]").click();
-                  setTimeout(function () {
-                    {
-                      var _0x4928b1 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-                        _0x4e06d7 = document.querySelectorAll("[class^=render-item]");
-                      _0x4e06d7[_0x1bbaaf].querySelector("[class^=talking-btn]").click();
-                    }
-                  }, 1200);
-                }
+              if (_0x58805b[_0x31b60c].value == index) {
+                var _0x42b4a8 = _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+                _0x42b4a8 == "开始讲解" ? _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click() : (_0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                  _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+                }, 2000));
                 break;
               }
             }
           }
-        }, 1000);
+        }, 800);
       } else {
         var indea = event.budget.spsort,
           asi = parseInt(arras[indea]),
           index = asi,
-          divheight = document.querySelector("[class^=render-item]").offsetHeight,
-          topdiv = document.querySelector("[class^=list-wrap]"),
-          asdiv = topdiv.querySelector("div");
+          divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+          asdiv = document.querySelector(".goods-wrapper");
         asdiv.scrollTop = divheight * index - divheight;
         setTimeout(function () {
-          var _0x2c26d0 = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-            _0x345e1b = document.querySelectorAll("[class^=render-item]");
-          for (var _0x1b1881 = 0; _0x1b1881 < _0x2c26d0.length; _0x1b1881++) {
-            {
-              if (_0x2c26d0[_0x1b1881].value == index) {
-                var _0x51ef72 = _0x345e1b[_0x1b1881].querySelector("[class^=operate-area]").textContent;
-                if (_0x51ef72 == "讲解") _0x345e1b[_0x1b1881].querySelector("[class^=talking-btn]").click();else {
-                  var _0x345e1b = document.querySelectorAll("[class^=render-item]");
-                  _0x345e1b[_0x1b1881].querySelector("[class^=talking-btn]").click();
-                  setTimeout(function () {
-                    var _0x3b42be = document.querySelectorAll(".okee-current-live-input.okee-current-live-input-size-md"),
-                      _0x21b68e = document.querySelectorAll("[class^=render-item]");
-                    _0x21b68e[_0x1b1881].querySelector("[class^=talking-btn]").click();
-                  }, 1200);
-                }
-                break;
-              }
+          var _0x24bbde = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+            _0x5677b3 = _0x24bbde.querySelectorAll("input"),
+            _0x4ebc75 = _0x24bbde.querySelectorAll(".live-pro-card-draggable");
+          for (var _0x18dcca = 0; _0x18dcca < _0x5677b3.length; _0x18dcca++) {
+            if (_0x5677b3[_0x18dcca].value == index) {
+              var _0xf008d1 = _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+              _0xf008d1 == "开始讲解" ? _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click() : (_0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+              }, 2000));
+              break;
             }
           }
-        }, 1000);
+        }, 800);
         chrome.storage.local.set({
           "spsort": event.budget.spsort + 1
         }, function () {});
