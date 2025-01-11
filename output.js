11b155e25906a25456e49d25d385f573af231030
action <action@github.com>
2025-01-11 03:40:24 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,91 +1,178 @@
-//Sat Jan 11 2025 03:04:56 GMT+0000 (Coordinated Universal Time)
+//Sat Jan 11 2025 03:40:24 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-var index = 0,
-  st = setInterval(function () {
-    index = index + 1;
-    var _0x3fdb0e = document.getElementsByClassName("K4ovmNwz").length;
-    if (index > 10 || _0x3fdb0e > 0) {
-      {
-        clearInterval(st);
-        var _0x17a816 = document.querySelector("div.AJBA3PN9"),
-          _0x4efceb = null;
-        for (var _0x1d4fe1 = 0, _0x24755f = Object.keys(_0x17a816); _0x1d4fe1 < _0x24755f.length; _0x1d4fe1++) {
-          if (_0x4efceb = _0x24755f[_0x1d4fe1].match(/^__react[^$]*(\$.+)$/)) {
-            {
-              _0x4efceb = _0x4efceb[1];
+if (event.budget.kjstatus === 1) {
+  var arr = event.budget.kjtxt.split(/[(\r\n)\r\n]+/);
+  if (event.budget.time1 >= event.budget.kjtime) {
+    chrome.storage.local.set({
+      "time1": 0
+    }, function () {});
+    var num = arr.length;
+    if (event.budget.kjsort == 2) {
+      var randomNum = Math.floor(Math.random() * num) + 1;
+      num > 1 ? randomNum == event.budget.sendid && (randomNum < num ? randomNum += 1 : randomNum = 1) : randomNum = 1;
+      chrome.storage.local.set({
+        "sendid": randomNum
+      }, function () {});
+      var element_input = document.querySelector(".send-comment-area input");
+      element_input.click();
+      if (element_input._valueTracker) var _templMethod = element_input._valueTracker.getValue;
+      if (element_input.getValue) var _templMethod = element_input.getValue;
+      element_input._valueTracker && (element_input._valueTracker.getValue = () => "");
+      if (typeof arr[randomNum - 1] === "undefined") element_input.value = "";else {
+        element_input.value = hj(arr[randomNum - 1]);
+        var eventt = new InputEvent("input", {
+          "bubbles": true,
+          "cancelable": true
+        });
+        element_input.dispatchEvent(eventt);
+        element_input._valueTracker && (element_input._valueTracker.getValue = _templMethod);
+        setTimeout(function () {
+          document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+            "bubbles": true
+          }));
+        }, 200);
+      }
+    } else event.budget.sort >= num ? chrome.storage.local.set({
+      "sort": 1
+    }, function () {
+      var _0xaca04d = document.querySelector(".send-comment-area input");
+      _0xaca04d.click();
+      if (_0xaca04d._valueTracker) var _0x27b0c8 = _0xaca04d._valueTracker.getValue;
+      if (_0xaca04d.getValue) var _0x27b0c8 = _0xaca04d.getValue;
+      _0xaca04d._valueTracker && (_0xaca04d._valueTracker.getValue = () => "");
+      if (typeof arr[0] === "undefined") {} else {
+        _0xaca04d.value = hj(arr[0]);
+        var eventt = new InputEvent("input", {
+          "bubbles": true,
+          "cancelable": true
+        });
+        _0xaca04d.dispatchEvent(eventt);
+        _0xaca04d._valueTracker && (_0xaca04d._valueTracker.getValue = _0x27b0c8);
+        setTimeout(function () {
+          document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+            "bubbles": true
+          }));
+        }, 200);
+      }
+    }) : chrome.storage.local.set({
+      "sort": event.budget.sort + 1
+    }, function () {
+      var _0x500e16 = document.querySelector(".send-comment-area input");
+      _0x500e16.click();
+      if (_0x500e16._valueTracker) {
+        var _0x4949e5 = _0x500e16._valueTracker.getValue;
+      }
+      if (_0x500e16.getValue) {
+        var _0x4949e5 = _0x500e16.getValue;
+      }
+      if (_0x500e16._valueTracker) {
+        _0x500e16._valueTracker.getValue = () => "";
+      }
+      if (typeof arr[event.budget.sort] === "undefined") {} else {
+        {
+          _0x500e16.value = hj(arr[event.budget.sort]);
+          var eventt = new InputEvent("input", {
+            "bubbles": true,
+            "cancelable": true
+          });
+          _0x500e16.dispatchEvent(eventt);
+          _0x500e16._valueTracker && (_0x500e16._valueTracker.getValue = _0x4949e5);
+          setTimeout(function () {
+            document.querySelector(".send-comment-area button").dispatchEvent(new MouseEvent("click", {
+              "bubbles": true
+            }));
+          }, 200);
+        }
+      }
+    });
+  } else chrome.storage.local.set({
+    "time1": event.budget.time1 + 3
+  }, function () {});
+}
+if (event.budget.spstatus === 1) {
+  if (event.budget.time2 >= event.budget.sptime) {
+    chrome.storage.local.set({
+      "time2": 0
+    }, function () {});
+    var arras = event.budget.spstr;
+    arras = arras.split(",");
+    if (arras.length == 1) {
+      var index = parseInt(arras[0]),
+        divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+        asdiv = document.querySelector(".goods-wrapper");
+      asdiv.scrollTop = divheight * index - divheight;
+      setTimeout(function () {
+        var _0x158f96 = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+          _0x27dc4a = _0x158f96.querySelectorAll("input"),
+          _0x1d4233 = _0x158f96.querySelectorAll(".live-pro-card-draggable");
+        for (var _0x2b3770 = 0; _0x2b3770 < _0x27dc4a.length; _0x2b3770++) {
+          {
+            if (_0x27dc4a[_0x2b3770].value == index) {
+              var _0x1c2ac0 = _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+              _0x1c2ac0 == "开始讲解" ? (console.log(_0x1c2ac0), _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click()) : (console.log(_0x1c2ac0), _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                console.log(_0x1c2ac0, 23);
+                _0x1d4233[_0x2b3770].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+              }, 2000));
               break;
             }
           }
         }
-        _0x4efceb = "__reactProps" + _0x4efceb;
-        var _0x1dbb8c = document.getElementsByClassName("AJBA3PN9"),
-          _0x47a5a4 = [],
-          _0x7b789a = "",
-          _0x219568 = 0,
-          _0x1ae9d5 = [],
-          urls = [],
-          _0x1f5cd5 = [],
-          _0xf68775 = "<table id=\"tableExcel\" width=\"100%\" border=\"1\" cellspacing=\"0\" cellpadding=\"0\"><tr><td colspan=\"5\" align=\"center\">采集商品列表</td></tr><tr><td>序号</td><td>名称</td><td>价格</td><td>链接</td></tr>";
-        for (var _0x1d4fe1 = 0; _0x1d4fe1 < _0x1dbb8c.length; _0x1d4fe1++) {
-          {
-            var _0x10fddd = "https://haohuo.jinritemai.com/ecommerce/trade/detail/index.html?id=" + _0x1dbb8c[_0x1d4fe1][_0x4efceb].children.props.children[0].props.promotion.product_id;
-            _0x1ae9d5[_0x1d4fe1] = _0x1dbb8c[_0x1d4fe1].querySelector("._RVz2Rea") ? _0x1dbb8c[_0x1d4fe1].querySelector("._RVz2Rea").textContent : "-";
-            urls[_0x1d4fe1] = _0x10fddd;
-            _0x1f5cd5[_0x1d4fe1] = _0x1dbb8c[_0x1d4fe1].querySelector(".CRfczKkD") ? _0x1dbb8c[_0x1d4fe1].querySelector(".CRfczKkD").textContent + "起" : "-";
-            _0xf68775 += "<tr><td>" + (_0x1d4fe1 + 1) + "</td><td>" + _0x1ae9d5[_0x1d4fe1] + "</td><td>" + _0x1f5cd5[_0x1d4fe1] + "</td><td>" + urls[_0x1d4fe1] + "</td></tr>";
-            _0x1d4fe1 % 30 === 0 ? (_0x219568 = _0x219568 + 1, _0x219568 > 1 ? _0x7b789a = _0x7b789a + ("</div><div style='background:#c1ecff;border: 1px solid red;display: inline-block;padding: 15px;right: 0;position: absolute;height: 50px;top:" + _0x219568 * 50 + "px;'" + " id='sp" + _0x219568 + "'>" + _0x219568 + "点击复制</div>" + _0x219568 + "段") + "<div class='duanlink' id='splink" + _0x219568 + "'>" + _0x10fddd + "<br>" : _0x7b789a = _0x7b789a + ("<div  style='background:#c1ecff;border: 1px solid red;display: inline-block;padding: 15px;right: 0;position: absolute;height: 50px;top:50px;' id='sp" + _0x219568 + "'>" + _0x219568 + "点击复制</div>" + _0x219568 + "段") + "<div id='splink" + _0x219568 + "'>" + _0x10fddd + "<br>") : _0x7b789a = _0x7b789a + _0x10fddd + "<br>";
-          }
-        }
-        _0x7b789a += "<div style=\"background:#c1ecff;border: 1px solid red;display: inline-block;padding: 15px;right: 0;position: absolute;height: 50px;top:0;\" id=\"all\">导出Excel</div>";
-        _0xf68775 += "</table>";
-        document.body.style.backgroundColor = "#ffffff";
-        document.body.style.height = "auto";
-        document.body.innerHTML = _0x7b789a;
-        var _0x1c7146 = document.getElementById("sp1");
-        _0x1c7146 && (_0x1c7146.onclick = function () {
-          copy("splink1");
-          _0x1c7146.innerHTML = "<span style='color:red;'>已复制1段<span>";
-        });
-        var _0x5895b0 = document.getElementById("sp2");
-        _0x5895b0 && (_0x5895b0.onclick = function () {
-          copy("splink2");
-          _0x5895b0.innerHTML = "<span style='color:red;'>已复制2段<span>";
-        });
-        var _0x35646f = document.getElementById("sp3");
-        _0x35646f && (_0x35646f.onclick = function () {
-          copy("splink3");
-          _0x35646f.innerHTML = "<span style='color:red;'>已复制3段<span>";
-        });
-        var _0x692d17 = document.getElementById("sp4");
-        _0x692d17 && (_0x692d17.onclick = function () {
-          copy("splink4");
-          _0x692d17.innerHTML = "<span style='color:red;'>已复制4段<span>";
-        });
-        var _0x9f1ccc = document.getElementById("sp5");
-        _0x9f1ccc && (_0x9f1ccc.onclick = function () {
-          copy("splink5");
-          _0x9f1ccc.innerHTML = "<span style='color:red;'>已复制5段<span>";
-        });
-        var _0x299b66 = document.getElementById("sp6");
-        if (_0x299b66) {
-          _0x299b66.onclick = function () {
-            copy("splink6");
-            _0x299b66.innerHTML = "<span style='color:red;'>已复制6段<span>";
-          };
-        }
-        var _0x47748c = document.getElementById("sp7");
-        _0x47748c && (_0x47748c.onclick = function () {
-          copy("splink7");
-          _0x47748c.innerHTML = "<span style='color:red;'>已复制7段<span>";
-        });
-        var _0x233869 = document.getElementById("all");
-        _0x233869.onclick = function () {
-          method5(_0xf68775);
-        };
-      }
+      }, 800);
     } else {
-      var _0x330bce = document.querySelector(".PGA7qLAy");
-      _0x330bce.scrollTop = _0x330bce.scrollHeight;
+      var nums = arras.length - 1;
+      let nows = new Date();
+      if (event.budget.spsort > nums) {
+        chrome.storage.local.set({
+          "spsort": 1
+        }, function () {});
+        var index = parseInt(arras[0]),
+          divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+          asdiv = document.querySelector(".goods-wrapper");
+        asdiv.scrollTop = divheight * index - divheight;
+        setTimeout(function () {
+          var _0x446428 = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+            _0x58805b = _0x446428.querySelectorAll("input"),
+            _0x20386c = _0x446428.querySelectorAll(".live-pro-card-draggable");
+          for (var _0x31b60c = 0; _0x31b60c < _0x58805b.length; _0x31b60c++) {
+            {
+              if (_0x58805b[_0x31b60c].value == index) {
+                var _0x42b4a8 = _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+                _0x42b4a8 == "开始讲解" ? _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click() : (_0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                  _0x20386c[_0x31b60c].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+                }, 2000));
+                break;
+              }
+            }
+          }
+        }, 800);
+      } else {
+        var indea = event.budget.spsort,
+          asi = parseInt(arras[indea]),
+          index = asi,
+          divheight = document.querySelector(".goods-wrapper .live-pro-card-draggable").offsetHeight,
+          asdiv = document.querySelector(".goods-wrapper");
+        asdiv.scrollTop = divheight * index - divheight;
+        setTimeout(function () {
+          var _0x24bbde = document.querySelectorAll(".goods-wrapper .live-pro-card>div")[1],
+            _0x5677b3 = _0x24bbde.querySelectorAll("input"),
+            _0x4ebc75 = _0x24bbde.querySelectorAll(".live-pro-card-draggable");
+          for (var _0x18dcca = 0; _0x18dcca < _0x5677b3.length; _0x18dcca++) {
+            if (_0x5677b3[_0x18dcca].value == index) {
+              var _0xf008d1 = _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").textContent.trim();
+              _0xf008d1 == "开始讲解" ? _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click() : (_0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click(), setTimeout(function () {
+                _0x4ebc75[_0x18dcca].querySelector(".live-pro-card-draggable-item-content-price-btn button").click();
+              }, 2000));
+              break;
+            }
+          }
+        }, 800);
+        chrome.storage.local.set({
+          "spsort": event.budget.spsort + 1
+        }, function () {});
+      }
     }
-  }, 1200);
+  } else chrome.storage.local.set({
+    "time2": event.budget.time2 + 3
+  }, function () {});
+}
