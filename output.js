7721dd6f1543a901bab0f60646ead08a685eb6a0
action <action@github.com>
2025-01-11 03:54:54 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,186 +1,44 @@
-//Sat Jan 11 2025 03:53:48 GMT+0000 (Coordinated Universal Time)
+//Sat Jan 11 2025 03:54:54 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function sendzbjfun(_0x416cdd, _0x5f0759) {
-  var _0x485b9e = document.getElementsByClassName("auxo-input-borderless")[0];
-  if (_0x485b9e._valueTracker) {
-    var _0x43c931 = _0x485b9e._valueTracker.getValue;
-  }
-  if (_0x485b9e.getValue) var _0x43c931 = _0x485b9e.getValue;
-  _0x485b9e._valueTracker && (_0x485b9e._valueTracker.getValue = () => "");
-  if (typeof _0x416cdd[_0x5f0759 - 1] === "undefined") _0x485b9e.value = "";else {
-    _0x485b9e.value = hj(_0x416cdd[_0x5f0759 - 1]);
-    var eventt = new InputEvent("input", {
-      "bubbles": true,
-      "cancelable": true
-    });
-    _0x485b9e.dispatchEvent(eventt);
-    _0x485b9e._valueTracker && (_0x485b9e._valueTracker.getValue = _0x43c931);
-    setTimeout(function () {
-      var _0x32db9b = document.querySelectorAll("[class^=index-module__input]").length > 0 ? document.querySelectorAll("[class^=index-module__input]") : document.querySelectorAll("[class^=inputSuffix]");
-      for (let _0xfd4c = 0; _0xfd4c < _0x32db9b.length; _0xfd4c++) {
-        _0x32db9b[_0xfd4c].innerText == "发布" && _0x32db9b[_0xfd4c].dispatchEvent(new MouseEvent("click", {
-          "bubbles": true
-        }));
-      }
-    }, 200);
-  }
-}
-function sendsp(_0x4675ee) {
-  if (document.querySelector(".index__goodsItem___38cLa")) {
-    var _0x122053 = document.querySelector(".index__goodsItem___38cLa").offsetHeight + 8;
-  } else {
-    if (document.querySelector(".index__goodsItem___1vVP9")) var _0x122053 = document.querySelector(".index__goodsItem___1vVP9").offsetHeight + 8;else {
-      if (document.querySelectorAll("[class^=goodsItem-]").length > 0) var _0x122053 = document.querySelector("[class^=goodsItem-]").offsetHeight + 8;
-    }
-  }
-  if (document.querySelector(".index__goodsList___3XeWQ")) var _0x423b82 = document.getElementsByClassName("index__goodsList___3XeWQ")[0];else {
-    if (document.querySelector(".index__goodsList___3cUwi")) {
-      var _0x423b82 = document.getElementsByClassName("index__goodsList___3cUwi")[0];
-    } else {
-      if (document.querySelector("#live-control-goods-list-container")) {
-        var _0x423b82 = document.querySelector("#live-control-goods-list-container");
-      }
-    }
-  }
-  var _0x197505 = _0x423b82.querySelector("div");
-  _0x197505.scrollTop = _0x122053 * _0x4675ee - _0x122053;
-  setTimeout(function () {
-    {
-      if (document.querySelector(".index__goodsItem___38cLa")) var _0x1a0d3d = document.querySelectorAll(".index__goodsItem___38cLa .auxo-input"),
-        _0x49787f = document.querySelectorAll(".index__goodsItem___38cLa");else {
-        if (document.querySelector(".index__goodsItem___1vVP9")) var _0x1a0d3d = document.querySelectorAll(".index__goodsItem___1vVP9 .auxo-input"),
-          _0x49787f = document.querySelectorAll(".index__goodsItem___1vVP9");else {
-          if (document.querySelectorAll("[class^=goodsItem-]").length > 0) {
-            var _0x1a0d3d = document.querySelectorAll("[class^=goodsItem-] .auxo-input"),
-              _0x49787f = document.querySelectorAll("[class^=goodsItem-]");
-          }
-        }
-      }
-      for (var _0x535264 = 0; _0x535264 < _0x1a0d3d.length; _0x535264++) {
-        {
-          if (_0x1a0d3d[_0x535264].value == _0x4675ee) {
-            {
-              var _0x1f88ab = _0x49787f[_0x535264].querySelectorAll("button");
-              for (var _0xf76540 = 0; _0xf76540 < _0x1f88ab.length; _0xf76540++) {
-                if (_0x1f88ab[_0xf76540].textContent == "讲解") {
-                  _0x1f88ab[_0xf76540].click();
-                  break;
-                } else {
-                  if (_0x1f88ab[_0xf76540].textContent == "取消讲解") {
-                    {
-                      _0x1f88ab[_0xf76540].click();
-                      setTimeout(function () {
-                        _0x1f88ab[_0xf76540].click();
-                      }, 2200);
-                      break;
-                    }
-                  }
+var currentUrl = window.location.href,
+  targetContent = "live.douyin.com";
+setTimeout(function () {
+  if (currentUrl.includes(targetContent)) {
+    if (document.readyState === "complete") {
+      {
+        var _0x3fed9c = document.querySelector(".bYaAzfVn"),
+          _0x1a0c50 = document.querySelector("#MS3tMtRG");
+        if (_0x3fed9c || _0x1a0c50) {} else {
+          chrome.storage.local.set({
+            "cjid": "",
+            "cjstatus": false
+          }, function () {
+            var _0x22d5fc = document.querySelector(".U6vVCppM") ? 1 : 0,
+              _0x52c1eb = document.getElementsByClassName("bYaAzfVn");
+            if (_0x22d5fc) var _0x534d31 = document.getElementsByClassName("aktymjet");else var _0x534d31 = document.getElementsByClassName("D77MbyHc");
+            if (_0x52c1eb.length == 0 && _0x534d31.length > 0) {
+              const url = chrome.runtime.getURL("js/base.js"),
+                _0x35fe53 = document.createElement("script");
+              _0x35fe53.src = url;
+              document.head.appendChild(_0x35fe53);
+            } else {
+              if (_0x52c1eb.length == 0 && _0x534d31.length == 0) {
+                if (_0x22d5fc) document.querySelector(".U6vVCppM").click();else {
+                  document.querySelector(".q4OfieDg").click();
                 }
+                const url = chrome.runtime.getURL("js/base.js"),
+                  _0x286449 = document.createElement("script");
+                _0x286449.src = url;
+                document.head.appendChild(_0x286449);
               }
-              break;
             }
-          }
+          });
         }
       }
     }
-  }, 1000);
-}
-event.budget.zdtxt && event.budget.zdtime > 110 ? chrome.storage.local.set({
-  "zdtime": 0
-}, function () {
-  var _0x3f6b59 = document.getElementsByClassName("auxo-input-borderless")[0];
-  document.getElementsByClassName("auxo-checkbox-input")[0].click();
-  if (_0x3f6b59._valueTracker) var _0x132737 = _0x3f6b59._valueTracker.getValue;
-  if (_0x3f6b59.getValue) {
-    var _0x132737 = _0x3f6b59.getValue;
-  }
-  if (_0x3f6b59._valueTracker) {
-    _0x3f6b59._valueTracker.getValue = () => "";
-  }
-  _0x3f6b59.value = hj(event.budget.zdtxt);
-  var eventt = new InputEvent("input", {
-    "bubbles": true,
-    "cancelable": true
-  });
-  _0x3f6b59.dispatchEvent(eventt);
-  _0x3f6b59._valueTracker && (_0x3f6b59._valueTracker.getValue = _0x132737);
-  setTimeout(function () {
-    var _0x511060 = document.querySelectorAll("[class^=index-module__input]").length > 0 ? document.querySelectorAll("[class^=index-module__input]") : document.querySelectorAll("[class^=inputSuffix]");
-    for (var _0x113d92 = 0; _0x113d92 < _0x511060.length; _0x113d92++) {
-      _0x511060[_0x113d92].innerText == "发布" && _0x511060[_0x113d92].dispatchEvent(new MouseEvent("click", {
-        "bubbles": true
-      }));
-    }
-  }, 200);
-}) : chrome.storage.local.set({
-  "zdtime": event.budget.zdtime + 3
-}, function () {});
-if (event.budget.kjstatus === 1) {
-  if (event.budget.time1 >= event.budget.kjtime) {
-    chrome.storage.local.set({
-      "time1": 0
-    }, function () {});
-    if (parseInt(event.budget.kjcon) === 1) {
-      if (document.querySelector(".index-module__sendIcon___1N04F")) var num = document.getElementsByClassName("index-module__sendIcon___1N04F").length;else {
-        if (document.querySelector(".index-module__sendIcon___1OLWd")) var num = document.getElementsByClassName("index-module__sendIcon___1OLWd").length;
-      }
-    } else var arr = event.budget.kjtxt.split(/[(\r\n)\r\n]+/),
-      num = arr.length;
-    if (event.budget.kjsort == 2) {
-      var randomNum = Math.floor(Math.random() * num) + 1;
-      num > 1 ? randomNum == event.budget.sendid && (randomNum < num ? randomNum += 1 : randomNum = 1) : randomNum = 1;
-      chrome.storage.local.set({
-        "sendid": randomNum
-      }, function () {});
-      if (parseInt(event.budget.kjcon) === 1) {
-        if (document.querySelector(".index-module__sendIcon___1N04F")) document.getElementsByClassName("index-module__sendIcon___1N04F")[randomNum - 1].click();else document.querySelector(".index-module__sendIcon___1OLWd") && document.getElementsByClassName("index-module__sendIcon___1OLWd")[randomNum - 1].click();
-      } else sendzbjfun(arr, randomNum);
-    } else event.budget.sort >= num ? chrome.storage.local.set({
-      "sort": 1
-    }, function () {
-      if (parseInt(event.budget.kjcon) === 1) {
-        if (document.querySelector(".index-module__sendIcon___1N04F")) document.getElementsByClassName("index-module__sendIcon___1N04F")[0].click();else document.querySelector(".index-module__sendIcon___1OLWd") && document.getElementsByClassName("index-module__sendIcon___1OLWd")[0].click();
-      } else {
-        sendzbjfun(arr, 1);
-      }
-    }) : chrome.storage.local.set({
-      "sort": event.budget.sort + 1
-    }, function () {
-      if (parseInt(event.budget.kjcon) === 1) {
-        if (document.querySelector(".index-module__sendIcon___1N04F")) document.getElementsByClassName("index-module__sendIcon___1N04F")[event.budget.sort].click();else document.querySelector(".index-module__sendIcon___1OLWd") && document.getElementsByClassName("index-module__sendIcon___1OLWd")[event.budget.sort].click();
-      } else sendzbjfun(arr, event.budget.sort + 1);
-    });
   } else chrome.storage.local.set({
-    "time1": event.budget.time1 + 3
-  }, function () {});
-}
-if (event.budget.spstatus === 1) {
-  if (event.budget.time2 >= event.budget.sptime) {
-    chrome.storage.local.set({
-      "time2": 0
-    }, function () {});
-    var arraydy = event.budget.spstr;
-    arraydy = arraydy.split(",");
-    if (arraydy.length == 1) var index = parseInt(arraydy[0]);else {
-      var nums = arraydy.length - 1;
-      let nows = new Date();
-      if (event.budget.spsort > nums) {
-        chrome.storage.local.set({
-          "spsort": 1
-        }, function () {});
-        var index = parseInt(arraydy[0]);
-      } else {
-        var indea = event.budget.spsort,
-          asi = parseInt(arraydy[indea]),
-          index = asi;
-        chrome.storage.local.set({
-          "spsort": event.budget.spsort + 1
-        }, function () {});
-      }
-    }
-    sendsp(index);
-  } else chrome.storage.local.set({
-    "time2": event.budget.time2 + 3
-  }, function () {});
-}
+    "cjid": "",
+    "cjstatus": false
+  }, function () {}), alert("请输入正确的直播间网址，示例：https://live.douyin.com/110000，11000是抖音账号，直播间主页复制即可");
+}, 2000);
